343a009775c8839fd8e4c2ad1dfe4d2ccee7ede9
rimasushil <rimasushil220@gamil.com>
2026-09-11 00:16:09 +0600

    js

---
+++ js/arrays.js
@@ -0,0 +1,15 @@
+const students = ["Bill Gates", "Steve Jobs", "Mark Zuckerberg"];
+console.log(students[0]);
+console.log(students[1]);
+console.log(students.length);
+students.push("Elon Musk");
+students.pop();
+students.unshift("Elon Musk");
+students.shift();
+console.log(students);
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((number) => number * 2);
+console.log(doubled);
+const numbers2 = [10, 15, 20, 25, 30];
+const result = numbers2.filter((number) => number >= 20);
+console.log(result);
